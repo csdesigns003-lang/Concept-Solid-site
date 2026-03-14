b7897ed18fe046ffa7c4b86c50c9093938ea7107
csdesigns003-lang <csdesigns003@gmail.com>
2026-03-13 21:58:37 -0500

    Create animations.js

---
+++ js/animations.js
@@ -0,0 +1,15 @@
+document.querySelectorAll(".card").forEach(card=>{
+
+card.addEventListener("mouseover",()=>{
+
+card.style.transform="scale(1.05)"
+
+})
+
+card.addEventListener("mouseout",()=>{
+
+card.style.transform="scale(1)"
+
+})
+
+})
