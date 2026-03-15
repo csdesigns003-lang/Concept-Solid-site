54ead9fcb4a7510e8ed73c72d0c934ad4b6accea
csdesigns003-lang <csdesigns003@gmail.com>
2026-03-15 13:45:37 -0500

    Update auth.js

--- sapem/js/auth.js
+++ sapem/js/auth.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL"
-const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY"
+const SUPABASE_URL = "https://odantloiqtucdqoqwjnl.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kYW50bG9pcXR1Y2Rxb3F3am5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1ODc2NjMsImV4cCI6MjA4OTE2MzY2M30._NFnGZ2GlRSPrB37cRn5mVdvCdMe1MuGbI6SPSa44BU"
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
