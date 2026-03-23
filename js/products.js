fb2ef7f6ffda28d5100e95104d23c531854c49fe
csdesigns003-lang <csdesigns003@gmail.com>
2026-03-22 19:28:14 -0500

    Create products.js

---
+++ js/products.js
@@ -0,0 +1,31 @@
+async function loadProducts(dataFile, containerID) {
+
+const res = await fetch(dataFile)
+const products = await res.json()
+
+const grid = document.getElementById(containerID)
+
+products.forEach(product => {
+
+grid.innerHTML += `
+
+<a href="product.html?id=${product.id}"
+class="block bg-white rounded-xl shadow hover:shadow-xl p-6">
+
+<img src="${product.images[0]}" class="rounded mb-4">
+
+<h3 class="text-xl font-bold">
+${product.name}
+</h3>
+
+<p class="text-blue-700 font-semibold">
+$${product.price}
+</p>
+
+</a>
+
+`
+
+})
+
+}
