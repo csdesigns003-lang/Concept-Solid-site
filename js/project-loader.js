934a3c7c92aedf84576246667dafaa0e486a309d
csdesigns003-lang <csdesigns003@gmail.com>
2026-03-24 18:41:02 -0500

    Create project-loader.js

---
+++ js/project-loader.js
@@ -0,0 +1,51 @@
+async function loadProjects(dataFile, containerID){
+
+const res = await fetch(dataFile)
+const projects = await res.json()
+
+const grid = document.getElementById(containerID)
+
+projects.forEach(p=>{
+
+grid.innerHTML += `
+
+<a href="project.html?id=${p.id}"
+class="bg-white p-6 rounded-xl shadow hover:shadow-xl transition">
+
+<img src="${p.image}" class="rounded mb-4">
+
+<h3 class="text-xl font-bold mb-2">
+${p.name}
+</h3>
+
+<p class="text-gray-600 text-sm">
+Difficulty: ${p.difficulty}
+</p>
+
+</a>
+
+`
+
+})
+
+}
+
+async function loadProject(id){
+
+const res = await fetch("data/projects.json")
+const projects = await res.json()
+
+const p = projects.find(x=>x.id===id)
+
+document.getElementById("project-name").innerText = p.name
+document.getElementById("project-difficulty").innerText = p.difficulty
+document.getElementById("project-time").innerText = p.time
+document.getElementById("project-cost").innerText = p.cost
+document.getElementById("project-tools").innerText = p.tools
+document.getElementById("project-description").innerText = p.description
+
+document.getElementById("project-image").src = p.image
+
+document.getElementById("download-btn").href = p.zip
+
+}
