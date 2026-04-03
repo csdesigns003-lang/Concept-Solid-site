2662608ca6e4db5d7e0ce4b6a3040a21ebbdaf64
csdesigns003-lang <csdesigns003@gmail.com>
2026-04-03 17:10:01 -0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -29,3 +29,40 @@ $${product.price}
 })
 
 }
+
+
+
+async function loadComingSoon(dataFile, containerID){
+
+const res = await fetch(dataFile)
+const products = await res.json()
+
+const grid = document.getElementById(containerID)
+
+let html = ""
+
+products.forEach(product => {
+
+html += `
+
+<div class="bg-white rounded-xl shadow p-6 text-center opacity-80">
+
+<img src="${product.images[0]}" class="rounded mb-4">
+
+<h3 class="text-xl font-bold">
+${product.name}
+</h3>
+
+<p class="text-gray-500 font-semibold">
+Coming Soon
+</p>
+
+</div>
+
+`
+
+})
+
+grid.innerHTML = html
+
+}
