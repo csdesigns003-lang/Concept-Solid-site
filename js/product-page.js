b1829de4150021b23b2c86516e92939b5fd4b9f8
csdesigns003-lang <csdesigns003@gmail.com>
2026-04-11 10:51:33 -0500

    Update product-page.js

--- js/product-page.js
+++ js/product-page.js
@@ -10,7 +10,25 @@ const product = products.find(p => p.id === productID)
 
 document.getElementById("product-name").innerText = product.name
 document.getElementById("product-price").innerText = "$" + product.price
-document.getElementById("product-description").innerText = product.description
+
+const descriptionContainer = document.getElementById("product-description")
+
+product.description.forEach(line => {
+
+if(line === ""){
+descriptionContainer.innerHTML += `<div class="h-4"></div>`
+}
+
+else if(line === "FEATURES" || line === "HOW IT WORKS"){
+descriptionContainer.innerHTML += `<h3 class="text-xl font-semibold mt-6">${line}</h3>`
+}
+
+else{
+descriptionContainer.innerHTML += `<p class="mb-2">${line}</p>`
+}
+
+})
+  
 
 const mainImage = document.getElementById("main-image")
 mainImage.src = product.images[0]
