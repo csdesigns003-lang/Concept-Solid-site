c2a8704b9724bd6f7fef3497f0e529f3841c8330
csdesigns003-lang <csdesigns003@gmail.com>
2026-04-11 19:51:04 -0500

    Update project-loader.js

--- js/project-loader.js
+++ js/project-loader.js
@@ -1,55 +1,12 @@
+let currentProject = null
+
+
 async function loadProjects(dataFile, containerID){
 
 const res = await fetch(dataFile)
 const projects = await res.json()
 
 const grid = document.getElementById(containerID)
-
-const buttons = document.getElementById("project-buttons")
-
-buttons.innerHTML = ""
-
-// DOWNLOAD BUTTON
-if(project.download){
-buttons.innerHTML += `
-<button onclick="downloadProject()"
-class="bg-blue-600 hover:bg-blue-700
-text-white text-lg font-semibold
-px-8 py-4 rounded-xl shadow">
-
-⬇ Download Files
-
-</button>
-`
-}
-
-// EXTERNAL LINK
-if(project.external_link){
-buttons.innerHTML += `
-<a href="${project.external_link}" target="_blank"
-class="bg-green-600 hover:bg-green-700
-text-white text-lg font-semibold
-px-8 py-4 rounded-xl shadow">
-
-🔗 View Project
-
-</a>
-`
-}
-
-// VIDEO
-if(project.video){
-buttons.innerHTML += `
-<a href="${project.video}" target="_blank"
-class="bg-red-600 hover:bg-red-700
-text-white text-lg font-semibold
-px-8 py-4 rounded-xl shadow">
-
-▶ Watch Video
-
-</a>
-`
-}
 
 projects.forEach(p=>{
 
@@ -76,12 +33,17 @@ Difficulty: ${p.difficulty}
 
 }
 
+
+
 async function loadProject(id){
 
 const res = await fetch("data/projects.json")
 const projects = await res.json()
 
 const p = projects.find(x=>x.id===id)
+
+currentProject = p
+
 
 document.getElementById("project-name").innerText = p.name
 document.getElementById("project-difficulty").innerText = p.difficulty
@@ -92,18 +54,73 @@ document.getElementById("project-description").innerText = p.description
 
 document.getElementById("project-image").src = p.image
 
-document.getElementById("download-btn").href = p.zip
+
+// CREATE BUTTONS
+const buttons = document.getElementById("project-buttons")
+buttons.innerHTML = ""
+
+
+// DOWNLOAD BUTTON
+if(p.download){
+
+buttons.innerHTML += `
+<button onclick="downloadProject()"
+class="bg-blue-600 hover:bg-blue-700
+text-white text-xl font-semibold
+px-10 py-5 rounded-xl shadow-lg hover:shadow-xl transition">
+
+⬇ Download Files
+
+</button>
+`
+}
+
+
+// EXTERNAL LINK
+if(p.external_link){
+
+buttons.innerHTML += `
+<a href="${p.external_link}" target="_blank"
+class="bg-green-600 hover:bg-green-700
+text-white text-xl font-semibold
+px-10 py-5 rounded-xl shadow-lg hover:shadow-xl transition">
+
+🔗 View Project
+
+</a>
+`
+}
+
+
+// VIDEO
+if(p.video){
+
+buttons.innerHTML += `
+<a href="${p.video}" target="_blank"
+class="bg-red-600 hover:bg-red-700
+text-white text-xl font-semibold
+px-10 py-5 rounded-xl shadow-lg hover:shadow-xl transition">
+
+▶ Watch Video
+
+</a>
+`
+}
 
 }
+
 
 
 async function downloadProject(){
 
+if(!currentProject) return
+
 await recordDownload()
 
-window.location.href = "files/project-files.zip"
+window.location.href = currentProject.download
 
 }
+
 
 
 async function loadDownloads(){
